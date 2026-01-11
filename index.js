8517558e1e36c95161c0b9b108ec60e61e621d4f
omgabay <omer.gabay@yahoo.com>
2026-01-10 22:30:48 -0600

    Create express basic web server

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "Hello API" });
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
